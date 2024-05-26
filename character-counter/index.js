a5380fc6e97d32b04921ab0398a30c9d926b3ec1
NachoK00l <zeno77@outlook.be>
2024-05-26 16:14:15 +0200

    Added common words

--- character-counter/index.js
+++ character-counter/index.js
@@ -1,9 +1,39 @@
-var test = "hello";
+function getWordCount(list){
+    let count = list.reduce(function (value, value2) {
+        return (
+            value[value2] ? ++value[value2] :(value[value2] = 1),
+            value
+        );
+    }, {});
+    wordList = count;
+
+    return count;
+}
+
+function sortDictionary(dict) {
+    let sorted = Object.keys(dict).sort(function(a, b) {
+        return dict[b] - dict[a];
+    });
+    if (sorted.includes(" ")) {
+        sorted.splice(sorted.indexOf(" "), 1)
+    } else if (sorted.includes("")) {
+        sorted.splice(sorted.indexOf(""), 1)
+    }
+    return sorted;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function updateText() {
+
+    let wordListHolder = document.getElementById("wordList");
+    wordListHolder.innerHTML = "";
     
     updateCharacterCount();
     updateWordCount();
+    updateWordList();
 
 } 
 
@@ -33,4 +63,16 @@ function updateWordCount(){
     }
 
     wordLabel.innerHTML = wordCount;
+}
+
+function updateWordList() {
+    let wordListHolder = document.getElementById("wordList");
+    let textField = document.getElementById("input");
+
+    let wordList = getWordCount(textField.value.toLowerCase().split(" "));
+
+    for (item of sortDictionary(wordList).slice(0, 10)) {
+        wordListHolder.innerHTML += `
+        <p class="center">${capitalizeFirstLetter(item)}: ${wordList[item]}</p>`;
+    }
 }
