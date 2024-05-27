332df8f10c42240a4a5cb5789916bda49d892fd0
NachoK00l <96193203+NachoK00l@users.noreply.github.com>
2024-05-27 15:22:41 +0200

    Update index.js

--- yt-downloader/index.js
+++ yt-downloader/index.js
@@ -1,1 +1,13 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
+function getVideo(){
+
+  let url = document.getElementById("youtubeUrl").value;
+  let formatSelector = document.getElementById("format");
+
+  let selectedFormat = formatSelector.options[formatSelector].text;
+
+  ytdl(url, { filter: format => format.container === selectedFormat })
+  
+}
