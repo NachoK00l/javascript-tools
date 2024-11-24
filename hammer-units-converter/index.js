0e1f606eee7194a648896611647ae4ae4f3af287
NachoK00l <zeno77@outlook.be>
2024-11-24 09:16:15 +0100

    Added relative scaling

--- hammer-units-converter/index.js
+++ hammer-units-converter/index.js
@@ -1,5 +1,7 @@
 var huPerMeter = 39.6278;
 
+var gridSize = 64;
+
 function unitChanged(){
     var unit = document.getElementById("unit-select").value;
 
@@ -14,11 +16,20 @@ function unitChanged(){
 function metersUpdated(){
     var meters = document.getElementById("meters").value;
     var hu = meters * huPerMeter;
+    var scale = hu / gridSize;
     document.getElementById("hammers").value = hu;
+    document.getElementById("scale").value = scale;
 }
 
 function hammersUpdated(){
     var hammers = document.getElementById("hammers").value;
     var meters = hammers / huPerMeter;
+    var scale = hammers / gridSize;
     document.getElementById("meters").value = meters;
+    document.getElementById("scale").value = scale;
+}
+
+function gridChanged(){
+    var grid = document.getElementById("grid-select").value;
+    gridSize = grid;
 }
